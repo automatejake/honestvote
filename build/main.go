12b71678f2902dcf90c907be9a4426bc1b0fe0a1
jneubaum <jacob@neubaum.com>
2019-12-16 12:08:55 -0500

    envinronmental variables

--- build/main.go
+++ build/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jneubaum/honestvote/core/core-discovery/discovery"
 	"github.com/jneubaum/honestvote/core/core-http/http"
 	"github.com/jneubaum/honestvote/core/core-p2p/p2p"
+	"github.com/joho/godotenv"
 )
 
-//this file will be responsible for deploying the app
+//defaults
+var PEER_SERVICE string = "7000"
+var HTTP_SERVICE string = "7001"
 
+//this file will be responsible for deploying the app
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Loading ENV Failed")
+	}
+
+	// environmental variables override defaults
+	PEER_SERVICE = os.Getenv("PEER_SERVICE")
+	HTTP_SERVICE = os.Getenv("HTTP_SERVICE")
+
+	// accept optional flags that overrid environmental variables
 
 	// create http server
 	go http.CreateServer()
 
 	// search for connections
-	go discovery.FindPeer()
+	go discovery.FindPeer(PEER_SERVICE)
 
 	// accept incoming connections and handle p2p
-	p2p.ListenConn()
+	p2p.ListenConn(PEER_SERVICE)
 
 }
