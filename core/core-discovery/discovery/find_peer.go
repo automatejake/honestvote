1269a07f6f49d4425e706d964b951d8ff6e60dee
jneubaum <jacob@neubaum.com>
2019-12-16 11:53:54 -0500

    dial udp and fix env variable

--- core/core-discovery/discovery/find_peer.go
+++ core/core-discovery/discovery/find_peer.go
@@ -25,6 +25,11 @@ func FindPeer() {
 	ignore, _ := strconv.Atoi(os.Getenv("PORT"))
 	p2p.Nodes[ignore] = true
 
+	// conn, _ := net.DialUDP("udp", nil, &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 100, Zone: ""})
+	// defer conn.Close()
+	// conn.Write([]byte("hello"))
+	// conn.Read(make([]byte, 1024))
+
 	for {
 		for port := 7000; port <= 7001; port++ {
 			if !p2p.Nodes[port] {
