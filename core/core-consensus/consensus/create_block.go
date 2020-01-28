08d07e978a6f576a4fe3202928f7bac8f670461c
Matt Spencer <spence3645@gmail.com>
2020-01-28 10:31:04 -0500

    Moved all the proof-authority.go stuff to the new file create_block.go

--- core/core-consensus/consensus/create_block.go
+++ core/core-consensus/consensus/create_block.go
@@ -1,12 +1,22 @@
 package consensus
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/jneubaum/honestvote/core/core-crypto/crypto"
 	"github.com/jneubaum/honestvote/core/core-database/database"
 )
+
+func CalculateHash(input string) string {
+	hash := sha256.New()
+	hash.Write([]byte(input))
+	sum := hash.Sum(nil)
+	return base64.URLEncoding.EncodeToString(sum)
+}
 
 func GenerateBlock(prevBlock database.Block, transaction interface{}, pKey string) database.Block {
 	var newBlock database.Block
@@ -29,4 +39,24 @@ func CalculateMerkleRoot(transaction interface{}) string {
 	// switch t := block.Transaction.(type)
 	// case database.Vote
 	return ""
+}
+
+func VerifyHash(prevIndex int, prevHash string, block database.Block) bool {
+	if prevHash != block.PrevHash {
+		fmt.Println("Previous hash is wrong!")
+		return false
+	} else if CalculateHash(GenerateHeader(block)) != block.Hash {
+		fmt.Println("Block hash is wrong!", CalculateHash(GenerateHeader(block)))
+		return false
+	}
+
+	return true
+}
+
+func GenerateHeader(block database.Block) string {
+	var header string
+
+	header = string(block.Index) + block.Timestamp + block.PrevHash
+
+	return header
 }
--- scripts/deploy-local-chain.sh
+++ scripts/deploy-local-chain.sh
@@ -7,7 +7,7 @@ cd ../build
 go run main.go --tcp 7002 --http 7003 --role producer --collection-prefix a_ --registry true & \
 sleep 5
 go run main.go --tcp 7004 --http 7005 --role producer --collection-prefix b_ --registry-host 127.0.0.1 --registry-port 7002  & \
-sleep 15
+sleep 5
 go run main.go --tcp 7006 --http 7007 --role producer --collection-prefix c_ --registry-host 127.0.0.1 --registry-port 7002  & \
 #sleep 5
 #go run main.go --tcp 7008 --http 7009 --role full --collection-prefix d_ --registry-host 127.0.0.1 --registry-port 7001 & \
