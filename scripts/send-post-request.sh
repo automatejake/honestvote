e209640481f5e16bef6d4747dcb64ecc1c92a141
Matt Spencer <spence3645@gmail.com>
2020-02-01 15:22:02 -0500

    New producers receive correct block types that is an object not an array

--- scripts/send-post-request.sh
+++ scripts/send-post-request.sh
@@ -1,31 +1,31 @@
 curl --header "Content-Type: application/json" --request POST --data '{
-    "type": "Election",
-    "electionName": "Student Government Elections",
-    "institutionName": "West Chester University",
-    "description": "Spring Elections",
-    "startDate": "Fri, 24 Jan 2020 19:53:32 EST",
-    "endDate": "Tue, 11 Aug 2020 19:53:32 EDT",
-    "emailDomain": "wcupa.edu",
-    "positions": [
-        {
-            "id": "demfrmeororev",
-            "displayName": "Student Government President",
-            "candidates": [
-                {
-                    "name": "John Doe",
-                    "key": "test1"
-                },
-                {
-                    "name": "Sarah Jennings",
-                    "key": "test2"
-                },
-                {
-                    "name": "Maximus Footless",
-                    "key": "test3"
-                }
-            ]
-        }
-    ],
-    "sender": "3059301306072a8648ce3d020106082a8648ce3d030107034200046a8cee3c7842a24d956d4d4ea10c6255e5f57ec7606e17380cc155781819e9749be050a620e64c2e76152cdea00384cc352f6b28251e8394211173f6038e0f5e",
-    "id": "7b2252223a39343931363134343030313031383032393434343933393131333937393334393438323631333234393737303735343331383137333632363634323539343634323435343436353831313235352c2253223a36383834343438323939343837333639303439343234373935323036383934353734383632353933343736343833323431383033353130313333323733373639373032313136393434313338367d"
+	"type": "Election",
+	"electionName": "Student Government Elections",
+	"institutionName": "West Chester University",
+	"description": "Spring Elections",
+	"startDate": "Sat, 01 Feb 2020 14:27:27 EST",
+	"endDate": "Wed, 19 Aug 2020 14:27:27 EDT",
+	"emailDomain": "wcupa.edu",
+	"positions": [
+		{
+			"id": "demfrmeororev",
+			"displayName": "Student Government President",
+			"candidates": [
+				{
+					"name": "John Doe",
+					"key": "test1"
+				},
+				{
+					"name": "Sarah Jennings",
+					"key": "test2"
+				},
+				{
+					"name": "Maximus Footless",
+					"key": "test3"
+				}
+			]
+		}
+	],
+	"sender": "3059301306072a8648ce3d020106082a8648ce3d030107034200047c1d5cd365f84677317b8aa975421ad08d1242b66d5163534e9e20f814e40a14e464f300658aaf0ff5e1551bf805290ba6c7ca4ee3b0b0c8abf920fe51861ca6",
+	"signature": "7b2252223a31323438363239303436343733343938313136373238313537343935363936393034353338383436353239323732333131373239393330323534343331363534333530363738313734373236312c2253223a33373031373830383331333631393333323731333034373635323039393235343532353537323530373039383633323734383034363536373935363233343037333138313031343430363139347d"
 }' http://localhost:7003/election
