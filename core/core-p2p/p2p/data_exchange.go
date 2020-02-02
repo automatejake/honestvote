92ba46a9404a197eeb070755c53b3cb1d20ac619
Matt Spencer <spence3645@gmail.com>
2020-02-02 17:15:14 -0500

    Checking to see if block sent to new producer is election, if so then more needs to be done to transfer block.

--- core/core-p2p/p2p/data_exchange.go
+++ core/core-p2p/p2p/data_exchange.go
@@ -54,33 +54,35 @@ func GrabDocuments(client *mongo.Client, conn net.Conn, old_index string) {
 			if tran, ok := block.Transaction.(primitive.D); ok {
 				tranMap := tran.Map()
 
-				if pos, ok := tranMap["positions"].(primitive.A); ok {
-					var posElements primitive.A
-					tranMap["positions"] = nil
+				if tranMap["type"].(string) == "Election" {
+					if pos, ok := tranMap["positions"].(primitive.A); ok {
+						var posElements primitive.A
+						tranMap["positions"] = nil
 
-					for _, position := range pos {
-						if posInfo, ok := position.(primitive.D); ok {
-							posMap := posInfo.Map()
+						for _, position := range pos {
+							if posInfo, ok := position.(primitive.D); ok {
+								posMap := posInfo.Map()
 
-							if cand, ok := posMap["candidates"].(primitive.A); ok {
-								var candElements primitive.A
-								posMap["candidates"] = nil
+								if cand, ok := posMap["candidates"].(primitive.A); ok {
+									var candElements primitive.A
+									posMap["candidates"] = nil
 
-								for _, candidate := range cand {
-									if candInfo, ok := candidate.(primitive.D); ok {
-										candMap := candInfo.Map()
-										candElements = append(candElements, candMap)
+									for _, candidate := range cand {
+										if candInfo, ok := candidate.(primitive.D); ok {
+											candMap := candInfo.Map()
+											candElements = append(candElements, candMap)
+										}
 									}
+
+									posMap["candidates"] = candElements
 								}
 
-								posMap["candidates"] = candElements
+								posElements = append(posElements, posMap)
 							}
-
-							posElements = append(posElements, posMap)
 						}
-					}
 
-					tranMap["positions"] = posElements
+						tranMap["positions"] = posElements
+					}
 				}
 
 				block.Transaction = tranMap
