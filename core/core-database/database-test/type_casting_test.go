56abb17a2d925de248561f95ff622fc6572fe65c
Matt Spencer <spence3645@gmail.com>
2020-03-28 12:49:34 -0400

    More database test files are done.

--- core/core-database/database-test/type_casting_test.go
+++ core/core-database/database-test/type_casting_test.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"testing"
+
+	"github.com/jneubaum/honestvote/core/core-database/database"
 	//"github.com/jneubaum/honestvote/core/core-database/database"
 )
 
 func TestTransactionType(t *testing.T) {
+	var rType string
+
+	rType = database.TransactionType(database.Vote{})
+	if rType == "Vote" {
+		t.Log("Vote was given the correct type: ", rType)
+	} else {
+		t.Log("Vote was NOT given the correct type: ", rType)
+	}
+
+	rType = database.TransactionType(database.Registration{})
+	if rType == "Registration" {
+		t.Log("Registration was given the correct type: ", rType)
+	} else {
+		t.Log("Registration was NOT given the correct type: ", rType)
+	}
+
+	rType = database.TransactionType(database.Election{})
+	if rType == "Election" {
+		t.Log("Election was given the correct type: ", rType)
+	} else {
+		t.Log("Election was NOT given the correct type: ", rType)
+	}
 
 }
--- scripts/test-files.sh
+++ scripts/test-files.sh
@@ -4,4 +4,8 @@ cd ../core/core-database/database-test
 
 #go test -v database_exchange_test.go
 
-go test -v encoding_models_test.go
+#go test -v encoding_models_test.go
+
+#go test -v routing_tables_test.go
+
+go test -v type_casting_test.go
