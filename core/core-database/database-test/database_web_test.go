853a638197d1819efdcf3a4f92998d18b1e2c4ce
Matt Spencer <spence3645@gmail.com>
2020-03-28 12:14:50 -0400

    database_web_test.go is finished but will need to be edited later

--- core/core-database/database-test/database_web_test.go
+++ core/core-database/database-test/database_web_test.go
@@ -24,7 +24,7 @@ func TestGetElections(t *testing.T) {
 func TestGetElection(t *testing.T) {
 	database.MongoDB = database.MongoConnect("localhost")
 
-	election, err := database.GetElection("") //TODO: This shouldn't be an empty string
+	election, err := database.GetElection(" ") //TODO: This shouldn't be an empty string
 
 	if err != nil {
 		t.Error("There shouldn't be an error when grabbing elections from database. Error: ", err)
@@ -34,15 +34,48 @@ func TestGetElection(t *testing.T) {
 	t.Log("There was no error when grabbing the election from the database: ", election)
 }
 func TestGetVotes(t *testing.T) {
-	//	database.GetVotes()
+	database.MongoDB = database.MongoConnect("localhost")
 
+	votes, err := database.GetVotes(" ") //TODO: This shouldn't be an empty string
+
+	if err != nil {
+		t.Error("There shouldn't be an error when grabbing votes from database. Error: ", err)
+		return
+	}
+	if votes == nil {
+		t.Log("No votes were returned when grabbing from the databasae, this could be an error.")
+	}
+
+	t.Log("There was no error when grabbing the votes from the database: ", votes)
 }
 func TestGetPermissions(t *testing.T) {
-	//	database.GetPermissions()
+	database.MongoDB = database.MongoConnect("localhost")
 
+	permissions, err := database.GetPermissions(" ") //TODO: This shouldn't be an empty string
+
+	if err != nil {
+		t.Error("There shouldn't be an error when grabbing permissions from database. Error: ", err)
+		return
+	}
+	if permissions == nil {
+		t.Log("No permissions were returned when grabbing from the databasae, this could be an error.")
+	}
+
+	t.Log("There was no error when grabbing the permissions from the database: ", permissions)
 }
 
-func GetEndPoint(t *testing.T) {
-	//	database.GetEndpoint()
+func TestGetEndPoint(t *testing.T) {
+	database.MongoDB = database.MongoConnect("localhost")
 
+	endpoint, err := database.GetEndpoint()
+
+	if err != nil {
+		t.Error("There shouldn't be an error when grabbing the endpoint from database. Error: ", err)
+		return
+	}
+	if endpoint == "" {
+		t.Log("No endpoint was returned when grabbing from the databasae, this could be an error.")
+	}
+
+	t.Log("There was no error when grabbing the endpoint from the database: ", endpoint)
 }
