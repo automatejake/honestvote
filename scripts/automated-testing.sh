9552685af1d4b9639344c4a95407925d6731b0f3
Matt Spencer <spence3645@gmail.com>
2020-04-11 14:02:59 -0400

    Automated testing shell script is filled out

--- scripts/automated-testing.sh
+++ scripts/automated-testing.sh
@@ -1,1 +1,47 @@
-../tests/automated-testing.sh
+cd ../core/core-administrator/administrator-test
+
+go test -v
+
+cd ../../core-consensus/consensus-test
+
+go test -v
+
+cd ../../core-crypto/crypto-test
+
+go test -v
+
+cd ../../core-database/database-test
+
+go test -v
+
+cd ../../core-discovery/discovery-test
+
+go test -v
+
+cd ../../core-election/election-test
+
+go test -v
+
+cd ../../core-http/http-test
+
+go test -v
+
+cd ../../core-p2p/p2p-test
+
+go test -v
+
+cd ../../core-registration/registration-test
+
+go test -v
+
+cd ../../core-registry/registry-test
+
+go test -v
+
+cd ../../core-validation/validation-test
+
+go test -v
+
+cd ../../core-websocket/websocket-test
+
+go test -v
