4acca2fbef07e7e0468d90510009b1d4c9e89e20
Matt Spencer <spence3645@gmail.com>
2020-06-18 18:36:16 -0400

    Merging with master

--- scripts/deploy-local-chain.sh
+++ scripts/deploy-local-chain.sh
@@ -1,9 +1,11 @@
-go run main.go --tcp 7002 --http 7003 --role producer --collection-prefix a_ --registry true --institution-name "Honestvote" --private-key "790bc20440d6dc17bff7a7b17547d4f5ea7782a4d30d4707878d5bb95cbd9676" --public-key "02e9e343da7bb7fbe2caf18315fa5c3f907f71c23caf9532accb225cb3aed1f6a3"  
+cd ../build
+
+go run main.go --tcp 7002 --http 7003 --role producer --collection-prefix a_ --registry true --institution-name "Honestvote" --private-key "2edfd293fd384a0036f519083c0636db111eb4d7949dd22927201e30d84755c8" --public-key "03effee3be38c8f30b1d29c4f7c4ec16b4e03d27599bb6c5efe0e26d42458358dc"  
 
 sleep 5
 
-go run main.go --tcp 7004 --http 7005 --role producer --collection-prefix b_ --registry-host 127.0.0.1 --registry-port 7002 --private-key "021f5d2717cc09d33fbbdee82d4abd95fbe365a80c84bc99ce4aa469697a21a7ab" --public-key "6514388d720c07b694377dc0236637e50e4a0cbb5c88d017d9df8c4f62c4ef10" --registry false & \
+go run main.go --tcp 7004 --http 7005 --role producer --collection-prefix b_ --registry-host 127.0.0.1 --registry-port 7002 --private-key "030168787ff44ecf8d8676db18da2ccacae85bb284607372a90987edc1c8d3d64a" --public-key "d41ff4f99b0737697e57ad7931926a978c76dcb06fc5727006212a8d2e9308fe" --registry false & \
 
 sleep 10
 
-go run main.go --tcp 7006 --http 7007 --role producer --collection-prefix c_ --registry-host 127.0.0.1 --registry-port 7002 --private-key "0215d5ed4faa64e879009ac16b8703f599f57d87ef47bc9b0c2b487c4013f25368" --public-key "697f14c7d4c13158a1ac6feb8550d599fbba8f85f0b09df0196d038259a05996" --registry false & \
+go run main.go --tcp 7006 --http 7007 --role producer --collection-prefix c_ --registry-host 127.0.0.1 --registry-port 7002 --private-key "03484041b1c8bc03fe134af7b643d310c3e2dd70d6fc0940d11aa17c0cd42eb97c" --public-key "301b5d78f02f565070b9d1b7aef978f7e8fd2c3473e1bdae2070eabd325ea6be" --registry false & \
