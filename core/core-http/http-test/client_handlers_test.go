a9ac5caf389b30f894df07650c3b8af7291b39c6
jneubaum <jacob@neubaum.com>
2020-07-01 09:31:35 -0400

    built skeleton for post route tests

--- core/core-http/http-test/client_handlers_test.go
+++ core/core-http/http-test/client_handlers_test.go
@@ -16,24 +16,68 @@ var MongoConnection = database.MongoConnect("localhost")
 
 //error because it must write a message to the administrator
 func TestPostRegisterHandler(t *testing.T) {
-	// jsonRegistration, _ := json.Marshal(database.AwaitingRegistration{})
-	// req, err := http.NewRequest("POST", "/register", bytes.NewBuffer(jsonRegistration))
+	// url := "http://127.0.0.1:7003"
+
+	// registration := database.Registration{}
+
+	// jsonStr, err := json.Marshal(&registration)
+	// req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	// if err != nil {
 	// 	t.Fatal(err)
 	// }
+	// req.Header.Set("Content-Type", "application/json")
 
 	// database.CollectionPrefix = CollectionPrefix
 	// database.MongoDB = MongoConnection
 
+	// // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 	// rr := httptest.NewRecorder()
 	// handler := http.HandlerFunc(corehttp.PostRegisterHandler)
 
+	// // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// // directly and pass in our Request and ResponseRecorder.
 	// handler.ServeHTTP(rr, req)
+
+	// t.Errorf("Request: %v", req)
+
+	// // Check the status code is what we expect.
+	// if status := rr.Code; status != http.StatusOK {
+	// 	t.Errorf("handler returned wrong status code: got %v want %v",
+	// 		status, http.StatusOK)
+	// }
 
 }
 
 func TestPostVoteHandler(t *testing.T) {
+	// url := "http://127.0.0.1:7003"
 
+	// vote := database.Vote{}
+
+	// jsonStr, err := json.Marshal(&vote)
+	// req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// req.Header.Set("Content-Type", "application/json")
+
+	// database.CollectionPrefix = CollectionPrefix
+	// database.MongoDB = MongoConnection
+
+	// // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	// rr := httptest.NewRecorder()
+	// handler := http.HandlerFunc(corehttp.PostVoteHandler)
+
+	// // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// // directly and pass in our Request and ResponseRecorder.
+	// handler.ServeHTTP(rr, req)
+
+	// t.Errorf("Request: %v", req)
+
+	// // Check the status code is what we expect.
+	// if status := rr.Code; status != http.StatusOK {
+	// 	t.Errorf("handler returned wrong status code: got %v want %v",
+	// 		status, http.StatusOK)
+	// }
 }
 
 func TestPostElectionHandler(t *testing.T) {
@@ -66,20 +110,6 @@ func TestPostElectionHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-
-	// Check the response body is what we expect.
-	// response := corehttp.Payload{}
-	// _ = json.Unmarshal(rr.Body.Bytes(), &response)
-
-	// expected := "OK"
-	// if response.Status != expected {
-	// 	t.Errorf("handler returned unexpected status: got %v want %v",
-	// 		response.Status, expected)
-	// }
-
-	// responseData, _ := json.Marshal(&response.Data)
-	// elections := []database.ElectionInfo{}
-	// json.Unmarshal(responseData, &elections)
 
 }
 
